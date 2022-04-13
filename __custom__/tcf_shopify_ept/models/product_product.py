1694fcb21f0412d0085f2c859fb5d4c1b3240943
André Schenkels <andre@schenkels.nl>
2022-04-13 13:09:08 +0000

    update

--- __custom__/tcf_shopify_ept/models/product_product.py
+++ __custom__/tcf_shopify_ept/models/product_product.py
@@ -1,13 +1,16 @@
+import logging
 from datetime import datetime
 from odoo.exceptions import Warning
 from odoo import models, fields, api
 
+_logger = logging.getLogger(__name__)
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def get_qty_on_hand(self, warehouse, product_list):
-        on_hand_products = self.env['product.product'].with_context(dict(self._context, warehouse=warehouse.id)).read_group([('id', 'in', product_list.ids)], ['id'], ['id', 'free_qty'])
+        on_hand_products = self.env['product.product'].with_context(dict(self._context, warehouse=warehouse.id)).read_group([('id', 'in', product_list.ids)], fields=['free_qty'])
+        _logger.info(on_hand_products)
         onhand_list = []
         # This is needed to keep the rest of the modules working
         for on_hand_product in on_hand_products:
@@ -15,7 +18,8 @@ class ProductProduct(models.Model):
         return onhand_list
 
     def get_forecated_qty(self, warehouse, product_list):
-        va_products = self.env['product.product'].with_context(dict(self._context, warehouse=warehouse.id)).read_group([('id', 'in', product_list.ids)], ['id'], ['id', 'virtual_available'])
+        va_products = self.env['product.product'].with_context(dict(self._context, warehouse=warehouse.id)).read_group([('id', 'in', product_list.ids)], fields=['virtual_available'])
+        _logger.info(va_products)
         va_list = []
         # This is needed to keep the rest of the modules working
         for va_product in va_products:
